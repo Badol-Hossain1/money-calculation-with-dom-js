460ad8f4138eb95479d4a407c0ca02e956dca617
Badol-hossain <badol@iiinigence.com>
2023-04-11 05:37:15 +0600

    all done

--- bank.js
+++ bank.js
@@ -1,20 +1,51 @@
-document.getElementById("total-income").addEventListener("click", function () {
-  const totalAmount = document.getElementById("total-amount");
-  const value = totalAmount.value;
-  const TotalvalueNum = parseInt(value);
-  const Food = document.getElementById("Food");
-  const FoodValue = Food.value;
-  const FoodNum = parseInt(FoodValue);
-  const Rent = document.getElementById("Rent");
-  const RentValue = Rent.value;
-  const RentNum = parseInt(RentValue);
-  const Clothes = document.getElementById("Clothes");
-  const ClothesValue = Clothes.value;
-  const ClothesNum = parseInt(ClothesValue);
+function getElementValueAsInt(elementId) {
+  const element = document.getElementById(elementId);
+  const value = element.value.trim(); // trim whitespace from input value
+  if (value === "") {
+    return null; // input value is empty, return null
+  }
+  const parsedValue = parseInt(value);
+  if (isNaN(parsedValue)) {
+    return null; // input value contains non-numeric characters, return null
+  } else {
+    return parsedValue;
+  }
+}
 
-  const sum = FoodNum + RentNum + ClothesNum 
-  const allSum =  TotalvalueNum - sum
-  console.log(allSum);
-  const totalSum = document.getElementById('totalSum')
-  totalSum.innerText = allSum
+document.getElementById("total-income").addEventListener("click", function () {
+  const totalAmount = getElementValueAsInt("total-amount");
+  const Food = getElementValueAsInt("Food");
+  const Rent = getElementValueAsInt("Rent");
+  const Clothes = getElementValueAsInt("Clothes");
+
+  if (isNaN(totalAmount) || isNaN(Food) || isNaN(Rent) || isNaN(Clothes)) {
+    return; // one of the values is NaN, return early
+  }
+
+  const sum = Food + Rent + Clothes;
+  const allSum = totalAmount - sum;
+  const totalSum = document.getElementById("totalSum");
+  const saveN = allSum * 0.1;
+
+  if (allSum < 0) {
+    return;
+  } else {
+    totalSum.innerText = allSum;
+  }
+});
+
+document.getElementById("saveId").addEventListener("click", function () {
+  const add = document.getElementById("save");
+  const totalAmount = getElementValueAsInt("total-amount");
+  const Food = getElementValueAsInt("Food");
+  const Rent = getElementValueAsInt("Rent");
+  const Clothes = getElementValueAsInt("Clothes");
+  if (isNaN(totalAmount) || isNaN(Food) || isNaN(Rent) || isNaN(Clothes)) {
+    return; // one of the values is NaN, return early
+  }
+
+  const sum = Food + Rent + Clothes;
+  const allSum = totalAmount - sum;
+  const saveN = allSum * 0.1;
+  add.innerText = saveN;
 });
